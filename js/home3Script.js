319aba7e2707d53d4da0a7d42bb3b73b833e242a
OstapHetman <prickly4528@gmail.com>
2017-10-23 12:55:55 +0300

    end home task 3

---
+++ js/home3Script.js
@@ -0,0 +1,37 @@
+function checkAge() {
+    let requiredAge = 18,
+        inputValueName = document.getElementById('name').value,
+        inputValueAge = +(document.getElementById('age').value),
+        yourAge = document.getElementById('yourAge'),
+        yourName = document.getElementById('yourName');
+
+    switch (true) {
+        case (inputValueAge === requiredAge):
+            yourAge.innerText = 'Congratulations ' + inputValueName + ' your age is valid ';
+            break;
+    }
+    switch (true) {
+        case (inputValueAge > requiredAge):
+            (yourAge.innerText = inputValueName + ' Your age exceeds ' + (inputValueAge - requiredAge) + ' year(s)');
+            break;
+    }
+    switch (true) {
+        case (inputValueAge < requiredAge && inputValueAge > 0):
+            (yourAge.innerText = inputValueName + ' You need wait ' + (requiredAge - inputValueAge) + ' year(s) to your 18');
+            break;
+    }
+    switch (true) {
+        case (inputValueAge == ''):
+            (yourAge.innerText = 'Enter your Age!');
+            break;
+            (yourAge.innerText = 'Enter a valid value ');
+            break;
+    }
+
+    // Helper Function
+    function checkName() {
+        (inputValueName.length == 0) ? yourName.innerText = 'Enter your Name!': yourName.innerText = '';
+    }
+
+    checkName();
+}
