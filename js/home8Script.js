77766c30fb3f58073057a61615de9778a0ffab20
OstapHetman <prickly4528@gmail.com>
2017-11-08 16:12:19 +0200

    finish homework-8

--- js/home8Script.js
+++ js/home8Script.js
@@ -1,52 +1,42 @@
- /* Events fired on the drag target */
+// Пользователь начинает перетаскивание элемента
+document.addEventListener("dragstart", function(e) {
+    e.dataTransfer.setData("Text", e.target.id); //добавляет данные в нужном формате
+    e.target.style.opacity = "0.5";
+    e.dataTransfer.effectAllowed = 'move';
+});
 
- document.addEventListener("dragstart", function(event) {
-     // The dataTransfer.setData() method sets the data type and the value of the dragged data
-     event.dataTransfer.setData("Text", event.target.id);
+// Пользователь отпускает курсор мыши в процессе перетаскивания
+document.addEventListener("dragend", function(e) {
+    e.target.style.opacity = "1";
+});
 
+// Перетаскиваемый элемент достигает конечного элемента
+document.addEventListener("dragenter", function(e) {
+    if (e.target.className == "droptarget") {
+        e.target.style.border = "7px dotted #d9534f";
+        e.target.style.cursor = "move";
+    }
+});
 
-     // Change the opacity of the draggable element
-     event.target.style.opacity = "0.4";
- });
+// Курсор мыши наведен на элемент при перетаскивании
+document.addEventListener("dragover", function(e) {
+    // При перетаскивании такого объекта, как ссылка, необходимо переопределить поведение браузера по умолчанию, т. е. переход по ней. Для этого нужно вызвать функцию e.preventDefault()
+    e.preventDefault();
+});
 
- document.addEventListener("dragend", function(event) {
-     event.target.style.opacity = "1";
- });
+//  курсор мыши покидает пределы перетаскиваемого элемента
+document.addEventListener("dragleave", function(e) {
+    if (e.target.className == "droptarget") {
+        event.target.style.border = "";
+    }
+});
 
-
- /* Events fired on the drop target */
-
- // When the draggable p element enters the droptarget, change the DIVS's border style
- document.addEventListener("dragenter", function(event) {
-     if (event.target.className == "droptarget") {
-         event.target.style.border = "3px dotted red";
-     }
- });
-
- // By default, data/elements cannot be dropped in other elements. To allow a drop, we must prevent the default handling of the element
- document.addEventListener("dragover", function(event) {
-     event.preventDefault();
-
- });
-
- // When the draggable p element leaves the droptarget, reset the DIVS's border style
- document.addEventListener("dragleave", function(event) {
-     if (event.target.className == "droptarget") {
-         event.target.style.border = "";
-     }
- });
-
- /* On drop - Prevent the browser default handling of the data (default is open as link on drop)
-    Reset the color of the output text and DIV's border color
-    Get the dragged data with the dataTransfer.getData() method
-    The dragged data is the id of the dragged element ("drag1")
-    Append the dragged element into the drop element
- */
- document.addEventListener("drop", function(event) {
-     event.preventDefault();
-     if (event.target.className == "droptarget") {
-         event.target.style.border = "";
-         var data = event.dataTransfer.getData("Text");
-         event.target.appendChild(document.getElementById(data));
-     }
- });
+// Происходит drop элемента
+document.addEventListener("drop", function(e) {
+    e.preventDefault();
+    if (e.target.className == "droptarget") {
+        e.target.style.border = "";
+        let data = e.dataTransfer.getData("Text"); // возвращает данные
+        e.target.appendChild(document.getElementById(data));
+    }
+});
