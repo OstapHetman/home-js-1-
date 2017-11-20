e38398ceb06d3808477c3b764d119f9d3c989dc2
OstapHetman <prickly4528@gmail.com>
2017-11-20 11:22:40 +0200

    implement display image onchange

--- js/getCars.js
+++ js/getCars.js
@@ -7,7 +7,8 @@ xmlhttp.onreadystatechange = function() {
         myArr = JSON.parse(this.responseText);
         var myCars = [];
         select = document.getElementById('carBrand');
-        // console.log(this.getAllResponseHeaders());
+        nextselect = document.getElementById('carColor');
+        nextselect2 = document.getElementById('carModel');
 
         for (let x of myArr['info']) {
             if (myCars.indexOf(x.brand) !== -1)
@@ -19,7 +20,12 @@ xmlhttp.onreadystatechange = function() {
             myCars.push(x.brand);
         }
 
-        select.addEventListener('change', getModels);
+        select.addEventListener('change', function() {
+            getModels();
+            getColors();
+        });
+        nextselect.addEventListener('change', getImages);
+        nextselect2.addEventListener('change', getImages);
 
     }
 };
@@ -28,12 +34,13 @@ xmlhttp.open("GET", url, true);
 xmlhttp.send();
 
 
-
+// Get Models Function
 function getModels() {
-    let modelSelect = document.getElementById('carModel');
-    let models = [];
+    let modelSelect = document.getElementById('carModel'),
+        modelCard = document.getElementsByClassName('carModel')[0],
+        models = [];
 
-    modelSelect.style.display = 'block';
+    modelCard.style.display = 'block';
     // el = colorSelect.firstChild;
     // colorSelect.innerHTML = '';
     // colorSelect.appendChild(el);
@@ -43,7 +50,6 @@ function getModels() {
 
             for (let x of JSON.parse(this.responseText)['info']) {
 
-
                 if (x.brand === document.getElementById('carBrand').value && models.indexOf(x.model) === -1) {
 
                     var opt = document.createElement('option');
@@ -52,14 +58,72 @@ function getModels() {
                     modelSelect.appendChild(opt);
 
                     models.push(x.model);
-
                 }
-
             }
         }
     }
-
-    xmlhttp.open("GET", url, true);
+    xmlhttp.open("GET", url, false);
     xmlhttp.send();
+}
 
+//Get Colors Function
+
+function getColors() {
+    let colorSelect = document.getElementById('carColor'),
+        colorCard = document.getElementsByClassName('carColor')[0],
+        colors = [];
+
+    colorCard.style.display = 'block';
+    xmlhttp.onreadystatechange = function() {
+
+        if (this.readyState == 4 && this.status == 200) {
+
+            for (let x of JSON.parse(this.responseText)['info']) {
+
+                if (x.brand === document.getElementById('carBrand').value && colors.indexOf(x.color) === -1) {
+
+                    var opt = document.createElement('option');
+                    opt.value = x.color;
+                    opt.innerHTML = x.color;
+                    colorSelect.appendChild(opt);
+
+                    colors.push(x.color);
+                }
+            }
+        }
+    }
+    xmlhttp.open("GET", url, false);
+    xmlhttp.send();
+}
+
+//Get Images Function
+
+function getImages() {
+    let imageDiv = document.getElementById('carImage');
+    let images = [];
+
+    imageDiv.style.display = 'block';
+    xmlhttp.onreadystatechange = function() {
+
+        if (this.readyState == 4 && this.status == 200) {
+
+            for (let x of JSON.parse(this.responseText)['info']) {
+
+                if (x.brand === document.getElementById('carBrand').value && images.indexOf(x.img) === -1) {
+                    var image = document.createElement('img');
+                    // image += '<img src="' + x.img + '">';
+                    image.src = x.img
+
+                    console.log(image);
+
+                    imageDiv.innerHTML = image;
+                    imageDiv.appendChild(image);
+
+                    images.push(x.img);
+                }
+            }
+        }
+    }
+    xmlhttp.open("GET", url, false);
+    xmlhttp.send();
 }
